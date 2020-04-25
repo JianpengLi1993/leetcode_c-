cc153d7988b99df92f78e07db0577fe26db3cea7
JianpengLi1993 <39554942+JianpengLi1993@users.noreply.github.com>
2020-04-25 18:16:13 +0800

    Update leetcode 40.固定数之和为固定值.cpp

--- leetcode 40.固定数之和为固定值.cpp
+++ leetcode 40.固定数之和为固定值.cpp
@@ -1,3 +1,82 @@
+// 原始的leetcode 40.
+给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的每个数字在每个组合中只能使用一次。
+
+说明：
+
+
+	所有数字（包括目标数）都是正整数。
+	解集不能包含重复的组合。 
+
+
+示例 1:
+
+输入: candidates = [10,1,2,7,6,1,5], target = 8,
+所求解集为:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+
+
+示例 2:
+
+输入: candidates = [2,5,2,1,2], target = 5,
+所求解集为:
+[
+  [1,2,2],
+  [5]
+]
+
+方案1：DFS递归。
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+
+private:
+	vector<int> candidates;
+	vector<vector<int>> res;
+	vector<int> path;
+public:
+	void DFS(int start, int target) {
+		if (target == 0) {
+			res.push_back(path);
+			return;
+		}
+
+		for (int i = start; i < candidates.size() && target - candidates[i] >= 0; i++) {
+			/*这里等效于下面的while循环作用
+            if (i > start&& candidates[i] == candidates[i - 1])
+			    continue;
+            */
+			path.push_back(candidates[i]);
+			// 元素不可重复利用，使用下一个即i+1
+			DFS(i + 1, target - candidates[i]);
+			path.pop_back();
+      while(i < candidates.size() - 1 && candidates[i] == candidates[i+1]){
+           i++;        
+      }
+		}
+	}
+
+	vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+		   sort(candidates.begin(), candidates.end());
+		   this->candidates = candidates;
+		   DFS(0, target);
+		   return res;
+	}
+};
+
+
+*40题的变形：组合的个数也有要求
+
+
 /*****************************************************************/
 https://www.toutiao.com/i6804264996227252743/?tt_from=weixin&utm_campaign=client_share&wxshare_count=1
 &timestamp=1586340756&app=news_article&utm_source=weixin&utm_medium=toutiao_ios&req_id=20200408181236010014041158082EC24E
