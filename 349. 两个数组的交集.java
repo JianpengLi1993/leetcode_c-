950e55fbc212eb7a9528401a5b068a21052853c0
JianpengLi1993 <39554942+JianpengLi1993@users.noreply.github.com>
2020-04-28 19:20:37 +0800

    Create 349. 两个数组的交集.java

---
+++ 349. 两个数组的交集.java
@@ -0,0 +1,151 @@
+难度简单177给定两个数组，编写一个函数来计算它们的交集。
+
+示例 1:
+输入: nums1 = [1,2,2,1], nums2 = [2,2]
+输出: [2]
+
+示例 2:
+输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出: [9,4]
+
+说明:
+	输出结果中的每个元素一定是唯一的。
+	我们可以不考虑输出结果的顺序。
+
+
+// version 1: sort & merge
+public class Solution {
+    /**
+     * @param nums1 an integer array
+     * @param nums2 an integer array
+     * @return an integer array
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int i = 0, j = 0;
+        int[] temp = new int[nums1.length];
+        int index = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                //这一句，第一个条件是为了防止越界，第二个条件是为了去重，即最终的temp数组中不能存在重复数字；
+                //若题目不要求去重，把这个条件拿掉，直接处理就行；
+                if (index == 0 || temp[index - 1] != nums1[i]) {
+                    temp[index++] = nums1[i];
+                }
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        int[] result = new int[index];
+        for (int k = 0; k < index; k++) {
+            result[k] = temp[k];
+        }
+        
+        return result;
+    }
+}
+
+// version 2: hash map
+public class Solution {
+    /**
+     * @param nums1 an integer array
+     * @param nums2 an integer array
+     * @return an integer array
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        
+        HashSet<Integer> hash = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            hash.add(nums1[i]);
+        }
+        
+        HashSet<Integer> resultHash = new HashSet<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (hash.contains(nums2[i]) && !resultHash.contains(nums2[i])) {
+                resultHash.add(nums2[i]);
+            }
+        }
+        
+        int size = resultHash.size();
+        int[] result = new int[size];
+        int index = 0;
+        for (Integer num : resultHash) {
+            result[index++] = num;
+        }
+        
+        return result;
+    }
+}
+
+// version 3: sort & binary search
+//先将短的数组进行排序O(nlogn)，再将另一个数组在排序好的数组中进行二分查找O(mlogn),整体的复杂度O((m+n)logn);
+public class Solution {
+    /**
+     * @param nums1 an integer array
+     * @param nums2 an integer array
+     * @return an integer array
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        Arrays.sort(nums1);
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                continue;
+            }
+            if (binarySearch(nums1, nums2[i])) {
+                set.add(nums2[i]);
+            }
+        }
+        
+        int[] result = new int[set.size()];
+        int index = 0;
+        for (Integer num : set) {
+            result[index++] = num;
+        }
+        
+        return result;
+    }
+    
+    private boolean binarySearch(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (nums[start] == target) {
+            return true;
+        }
+        if (nums[end] == target) {
+            return true;
+        }
+        
+        return false;
+    }
+}
