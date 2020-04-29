9f3343995e23769e26b270e3a31ad4456cc491c2
JianpengLi1993 <39554942+JianpengLi1993@users.noreply.github.com>
2020-04-29 23:21:32 +0800

    Update 拓扑排序及相关变形.cpp

--- 拓扑排序及相关变形.cpp
+++ 拓扑排序及相关变形.cpp
@@ -66,39 +66,168 @@ public:
 还要判断这个拓扑序列与org是否一一相等。
 https://www.lintcode.com/problem/sequence-reconstruction/。
 答案：
-https://www.jianshu.com/p/cf0bc9a6f252。
-
-
-        // 因为是唯一构成，所以每个元素的先序应该只有一个，即度为1
-        // 只有每次queue中只有一个元素的时候，产生的序列才是唯一的
-        while (q.size() == 1) {
-            int ele = q.poll();
-            for (int next : map.get(ele)) {
-                indegree.put(next, indegree.get(next) - 1);
-                if (indegree.get(next) == 0) {
-                    q.add(next);
+https://www.jiuzhang.com/solutions/sequence-reconstruction/#tag-other-lang-java。
+//本题构建的是有向图。
+class Solution {
+    /**
+     * @param org a permutation of the integers from 1 to n
+     * @param seqs a list of sequences
+     * @return true if it can be reconstructed only one or false
+     */
+    public:
+        
+        bool sequenceReconstruction(vector<int> &org, vector<vector<int>> &seqs) {
+            // build graph
+            unordered_map<int, unordered_set<int>> graph = getGraph(seqs);
+            /**
+            * we need to build graph from seqs!! cause we are using seqs to try to build org
+            * 
+            */
+            // build indegree
+            unordered_map<int, int> indegree = getIndegree(graph);
+            
+            // get zero indegree, offer in queue
+            queue<int> queue;
+            for (auto& node : graph) {
+                if (indegree[node.first] == 0) {
+                    queue.push(node.first);
                 }
             }
-            // 判断新的序列每个位置和 org 相不相等
-            if (ele != org[cnt]) {
+            // we shoud have a container to compare it to org
+            vector<int> sequence;
+            // bfs
+            while (!queue.empty()) {
+                int size = queue.size();
+                //这个是关键
+                // 因为是唯一构成，所以每个元素的先序应该只有一个，即度为1
+                // 只有每次queue中只有一个元素的时候，产生的序列才是唯一的
+                if (size > 1) {
+                    return false;
+                }
+                int node = queue.front();
+                queue.pop();
+                sequence.push_back(node);
+                for (auto& neib : graph[node]) {
+                    indegree[neib]--;
+                    if (indegree[neib] == 0) {
+                        queue.push(neib);
+                    }
+                }
+            }
+            
+            if (sequence.size() != org.size()) { // first compare length, to prevent egde cases.
                 return false;
             }
-            cnt++;
+            
+            for (int i = 0; i < org.size(); i++) {
+                if (sequence[i] != org[i]) {
+                    return false;
+                }
+            }
+            
+            return true;
         }
+    
+    private:
+        unordered_map<int, unordered_set<int>> getGraph(vector<vector<int>> &seqs) {
+            unordered_map<int, unordered_set<int>> graph;
+            //给所有节点都建了map,
+            for (auto path : seqs) {
+                for (int node : path) {
+                    unordered_set<int>tmp;
+                    graph.insert(make_pair(node, tmp));
+                }
+            }
+            //接着赋值邻居；这里是通过遍历边的出发节点，
+            //将出发节点后跟着的节点作为邻居加入set,不同的path中可能有相同的边，所以要用set
+            for (auto path : seqs) {
+                //这里实际构建的是有向图，因此实际i从0到size-2;
+                for (int i = 0; i < path.size() - 1; i++) {
+                    graph[path[i]].insert(path[i+1]);
+                }
+            }
+            return graph;
+        }
+    
+    private: 
+        unordered_map<int, int> getIndegree(unordered_map<int, unordered_set<int>> graph) {
+            unordered_map<int, int> indegree;
 
+            for (auto& node : graph) {
+                indegree.insert(make_pair(node.first, 0));
+            }
+            for (auto& node : graph) {
+                for (auto& neib : node.second) {
+                    indegree[neib]=indegree[neib] + 1;
+                }
+            }
+            return indegree;
+        }
+};
 
+//课程表I
+class Solution {
+public:
+    /*
+     * @param numCourses: a total of n courses
+     * @param prerequisites: a list of prerequisite pairs
+     * @return: true if can finish all courses or false
+     */
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> inDegree(numCourses);
+        
+        // 建图,由于知道节点个数和下标关系，所以建图简单，包括入度关系可以用一个数组表示
+        for (auto edge: prerequisites) {
+            graph[edge.second].push_back(edge.first);
+            inDegree[edge.first]++;
+        }
+        
+        int numChoose = 0;
+        queue<int> que;
+        
+        // 将入度为 0 的编号加入队列
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                que.push(i);
+            }
+        }
+        
+        while (not que.empty()) {
+            int now = que.front();
+            que.pop();
+            numChoose++;
+            // 将每条边删去，如果入度降为 0，再加入队列
+            for (int nxt: graph[now]) {
+                inDegree[nxt]--;
+                if (inDegree[nxt] == 0) {
+                    que.push(nxt);
+                }
+            }
+        }
+        
+        return numChoose == numCourses;
+    }
+};
 
 ###############
 综上：
-思路：先把图建造好了用dict，然后把入度存到另一个dict里面。把入度为0的点拿出来放进queue 和 我们的拓扑序当中。使用bfs，从queue中拿出一个点for它的neighbor并且入度减 1。当任意一个neighbor减到0的时候就把他们放进queue和拓扑序列中。因为这个题考查的是有没有一个拓扑序，所以在每次while queue下面我们都要判断一下这个queue里面是不是只有一个元素。
+思路：先把图建造好了用dict，然后把入度存到另一个dict里面。把入度为0的点拿出来放进queue 和 我们的拓扑序当中。
+使用bfs，从queue中拿出一个点for它的neighbor并且入度减 1。当任意一个neighbor减到0的时候就把他们放进queue和拓扑序列中。
+因为这个题考查的是有没有一个拓扑序，所以在每次while queue下面我们都要判断一下这个queue里面是不是只有一个元素。
 时间复杂度：O(n*l)
 这个题和之前做过的 course schedul1，2. topological sort 很像
 sequence reconstruction：是否有且只有一个拓扑序
 course schedule 1: 问的是有没有一个拓扑序
 course schedule 2:让我们求出任意一个拓扑序
 topological sort：让我们求出任意一个拓扑序
-这些题目本质都是求拓扑序。不同点是在于topological 之间给了图和adjency list。我们只需要统计一下入度就可以。 但是剩下这三道题都是隐士图。在course schedule1 和course schedule2 这两道题目中题目暗示了dependent的关系并且又说了找出任意一个序列（不止有一个序列）这些条件暗示可以用拓扑排序去做。在sequence reconstruction中他给了一个序列。然后又给了几个子序列。问这几个子序列能不能生成那一个给定的序列，所以等于是变相问我能不能找到有且只有一个拓扑序。
-难点：我认为难点在于 course1，2 sequence reconstruction中怎么根据输入来建造图和统计入度。这个应该根据不同的数据结构进行变换。这两个做完了就是往queue 和 拓扑序里面放入度为0的点。使用bfs，从queue中拿出一个点for它的neighbor并且入度减 1。当任意一个neighbor减到0的时候就把他们放进queue和拓扑序列中。
+这些题目本质都是求拓扑序。不同点是在于topological 之间给了图和adjency list。我们只需要统计一下入度就可以。 但是剩下这三道题都是隐士图。
+在course schedule1 和course schedule2 这两道题目中题目暗示了dependent的关系并且又说了找出任意一个序列（不止有一个序列）
+这些条件暗示可以用拓扑排序去做。在sequence reconstruction中他给了一个序列。然后又给了几个子序列。问这几个子序列能不能生成那一个给定的序列，
+所以等于是变相问我能不能找到有且只有一个拓扑序。
+难点：我认为难点在于 course1，2 sequence reconstruction中怎么根据输入来建造图和统计入度。这个应该根据不同的数据结构进行变换。
+这两个做完了就是往queue 和 拓扑序里面放入度为0的点。使用bfs，从queue中拿出一个点for它的neighbor并且入度减 1。
+当任意一个neighbor减到0的时候就把他们放进queue和拓扑序列中。
 以后碰到这种类型的题目需要 1.判断出使用拓扑序的做法
 2.根据不同的输入创造图，并且统计每个点的入度，如果点是从0开始可以考虑用数组，如果不是0开始就用dict来存。
 3.套用bfs的套路解决问题
