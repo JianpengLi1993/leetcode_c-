366a1868b1b953469dc5954235e76ac23800afe1
JianpengLi1993 <39554942+JianpengLi1993@users.noreply.github.com>
2020-05-04 20:05:43 +0800

    Update 快速排序.cpp

--- 快速排序.cpp
+++ 快速排序.cpp
@@ -2,6 +2,8 @@
 * 1.pivot的位置，不能选start或end;
 * 2.当出现左left右right两个指针进行比较时，用<=作为循环结束条件;
 * 3.当出现A[..]与pivot比较时，应用<小于号.
+**4.特别注意，此方案的快速排序，排一次结果无法确保pivot与左右指针的最终位置关系，也无法确保pivot的左侧都小于等于他，也无法确保右侧都大于等于他
+而只能确定start到right的都小于等于pivot，left到end都大于等于pivot。所以递归区间是以两个指针做端点
 */
 
 public class Solution {
@@ -49,3 +51,45 @@ public class Solution {
         quickSort(A, left, end);// A[left ... end]
     }
 }
+
+
+因此还有一张快速排序方法：
+这种方法执行结束后：left=right,nums[left]=pivot,且start到left-1都小于等于pivot，left+1到end都大于等于pivot。
+所以递归区间是以pivot为端点（即left 或者 right）
+class Solution {
+
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        
+        partition(nums, 0, nums.size() - 1);
+        return nums;
+    }
+    void partition(vector<int>& nums, int l, int r) {
+        
+        if(l>=r){
+            return;
+        }
+        int left = l, right = r;
+        int pivot = nums[left];
+        //这里是小于，退出时left=right
+        while (left < right) {
+            //此时nums[left]=pivot，这里严格执行nums[right]大于等于pivot，只有小于才交换，与上一方法不同；
+            while (nums[left] <= nums[right] and left < right) {
+                right--;
+            }
+            
+            swap(nums[left], nums[right]);
+            //此时nums[right]=pivot，这里严格执行nums[left]小于等于pivot，只有大于才交换，与上一方法不同；
+            while (nums[left] <= nums[right] and left < right) {
+                left++;
+            }
+            swap(nums[left], nums[right]);
+        }
+        nums[left] = pivot;
+        
+        // 如果第 k 小在右侧，搜索右边的范围，否则搜索左侧。此时，left=right=pivot存放的位置，若left=k，这说明pivot刚好存放到了nums[k],则得到结果
+        partition(nums, l, left - 1);
+        partition(nums, left + 1, r);
+
+    }
+};
