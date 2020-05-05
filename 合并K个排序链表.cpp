ff0ad22928d23707ae9d6fabdce6198876b27e20
JianpengLi1993 <39554942+JianpengLi1993@users.noreply.github.com>
2020-05-05 20:09:11 +0800

    Update 合并K个排序链表.cpp

--- 合并K个排序链表.cpp
+++ 合并K个排序链表.cpp
@@ -147,4 +147,56 @@ public:
         return head;  
     }
 };
-
+方案4：
+与方案3类似，也是两两merge，但是区别在于，1-2,3-4,5-6,7-8，之间先merge，然后把这一轮生成的新的1、2、3、4个链表继续，1-2,3-4进行merge，
+这样的复杂度O(nlogk)
+  Java代码：
+  
+public class Solution {
+    /**
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
+     */
+    public ListNode mergeKLists(List<ListNode> lists) {  
+        if (lists == null || lists.size() == 0) {
+            return null;
+        }
+        
+        while (lists.size() > 1) {
+            List<ListNode> new_lists = new ArrayList<ListNode>();
+            for (int i = 0; i + 1 < lists.size(); i += 2) {
+                ListNode merged_list = merge(lists.get(i), lists.get(i+1));
+                new_lists.add(merged_list);
+            }
+            if (lists.size() % 2 == 1) {
+                new_lists.add(lists.get(lists.size() - 1));
+            }
+            lists = new_lists;
+        }
+        
+        return lists.get(0);
+    }
+    
+    private ListNode merge(ListNode a, ListNode b) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (a != null && b != null) {
+            if (a.val < b.val) {
+                tail.next = a;
+                a = a.next;
+            } else {
+                tail.next = b;
+                b = b.next;
+            }
+            tail = tail.next;
+        }
+        
+        if (a != null) {
+            tail.next = a;
+        } else {
+            tail.next = b;
+        }
+        
+        return dummy.next;
+    }
+}
